f1da9247416bf7fc17ce84a2a186348fa54ef67c
Toby Clemson <tobyclemson@gmail.com>
2011-02-12 18:57:09 +0000

    Toby: Adding tests for all, any and none.

--- src/test/java/org/smallvaluesofcool/misc/functional/EagerTest.java
+++ src/test/java/org/smallvaluesofcool/misc/functional/EagerTest.java
@@ -1,6 +1,5 @@
 package org.smallvaluesofcool.misc.functional;
 
-import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,9 +7,9 @@ import java.util.Collection;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.smallvaluesofcool.misc.Literals.listWith;
 
 public class EagerTest {
@@ -44,7 +43,117 @@ public class EagerTest {
         });
 
         // Then
-        Assert.assertThat(actual, Matchers.hasItems(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        Assert.assertThat(actual, hasItems(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    }
+
+    @Test
+    public void shouldReturnTrueIfAnyElementsSatisfyThePredicateFunction() {
+        // Given
+        List<Integer> inputNumbers = listWith(5, 10, 15, 20);
+
+        // When
+        Boolean result = Eager.any(inputNumbers, new Predicate<Integer>() {
+            @Override
+            public boolean matches(Integer item) {
+                return item > 15;
+            }
+        });
+
+        // Then
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void shouldReturnFalseIfNoElementsSatisfyThePredicateFunction() {
+        // Given
+        List<Integer> items = listWith(5, 10, 15, 20);
+
+        // When
+        Boolean result = Eager.any(items, new Predicate<Integer>() {
+            @Override
+            public boolean matches(Integer item) {
+                return item > 25;
+            }
+        });
+
+        // Then
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void shouldReturnTrueIfAllElementsSatisfyThePredicateFunction() {
+        // Given
+        List<String> items = listWith("dog", "cat", "fish", "budgie");
+
+        // When
+        Boolean result = Eager.all(items, new Predicate<String>() {
+            @Override
+            public boolean matches(String item) {
+                return item.length() > 2;
+            }
+        });
+
+        // Then
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void shouldReturnFalseIfAnyOfTheElementsDoNotSatisyThePredicateFunction() {
+        // Given
+        List<String> items = listWith("dog", "cat", "fish", "budgie");
+
+        // When
+        Boolean result = Eager.all(items, new Predicate<String>() {
+            @Override
+            public boolean matches(String item) {
+                return item.length() > 3;
+            }
+        });
+
+        // Then
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void shouldReturnTrueIfNoneOfTheElementsMatchesThePredicateFunction() {
+        // Given
+        List<Integer> items = listWith(1, 3, 5, 7);
+
+        // When
+        Boolean result = Eager.none(items, new Predicate<Integer>() {
+            @Override
+            public boolean matches(Integer item) {
+                return isEven(item);
+            }
+
+            private boolean isEven(Integer item) {
+                return item % 2 == 0;
+            }
+        });
+
+        // Then
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void shouldReturnFalseIfAnyOfTheElementsMatchesTePredicateFunction() {
+        // Given
+        List<Integer> items = listWith(1, 3, 6, 7);
+
+        // When
+        Boolean result = Eager.none(items, new Predicate<Integer>() {
+            @Override
+            public boolean matches(Integer item) {
+                return isEven(item);
+            }
+
+            private boolean isEven(Integer item) {
+                return item % 2 == 0;
+            }
+        });
+
+        // Then
+        assertThat(result, is(false));
     }
 
     @Test
