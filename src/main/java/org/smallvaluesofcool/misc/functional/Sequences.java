a9635ee17efc9fca956018553ece87dd71354fb1
Toby Clemson <tobyclemson@gmail.com>
2011-03-27 17:37:41 +0100

    Toby: Oops, forgot to add the files.

---
+++ src/main/java/org/smallvaluesofcool/misc/functional/Sequences.java
@@ -0,0 +1,21 @@
+package org.smallvaluesofcool.misc.functional;
+
+import org.smallvaluesofcool.misc.datastructures.IntegerRange;
+
+public class Sequences {
+    public static Iterable<Integer> increasingIntegers() {
+        return new IntegerRange(0, null, 1);
+    }
+
+    public static Iterable<Integer> increasingIntegersFrom(int startPoint) {
+        return new IntegerRange(startPoint, null, 1);
+    }
+
+    public static Iterable<Integer> decreasingIntegers() {
+        return new IntegerRange(0, null, -1);
+    }
+
+    public static Iterable<Integer> decreasingIntegersFrom(int startPoint) {
+        return new IntegerRange(startPoint, null, -1);
+    }
+}
