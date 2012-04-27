90bcf576b0a2a128418c091477de96a0a1fcb6a1
Toby Clemson <tobyclemson@gmail.com>
2012-04-27 15:27:42 -0700

    Adding Option<T> with test.

---
+++ src/main/java/org/javafunk/funk/monads/Option.java
@@ -0,0 +1,106 @@
+package org.javafunk.funk.monads;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.util.NoSuchElementException;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public abstract class Option<T> {
+    public static <T> Option<T> none() {
+        return new None<T>();
+    }
+
+    public static <T> Option<T> some(T value) {
+        return new Some<T>(value);
+    }
+
+    public static <T> Option<T> option(T value) {
+        if (value == null) {
+            return none();
+        }
+        return some(value);
+    }
+
+    public abstract Boolean hasValue();
+
+    public abstract T get();
+
+    public abstract Option<T> or(Option<T> other);
+
+    public abstract Option<T> orSome(T other);
+
+    public abstract Option<T> orOption(T other);
+
+    @Override
+    public boolean equals(Object other) {
+        return EqualsBuilder.reflectionEquals(this, other);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    private static class None<T> extends Option<T> {
+        @Override public Boolean hasValue() {
+            return true;
+        }
+
+        @Override public T get() {
+            throw new NoSuchElementException();
+        }
+
+        @Override public Option<T> or(Option<T> other) {
+            return checkNotNull(other);
+        }
+
+        @Override public Option<T> orSome(T other) {
+            return some(other);
+        }
+
+        @Override public Option<T> orOption(T other) {
+            return option(other);
+        }
+
+        @Override
+        public String toString() {
+            return "Option::None[]";
+        }
+    }
+
+    private static class Some<T> extends Option<T> {
+        private final T value;
+
+        public Some(T value) {
+            this.value = value;
+        }
+
+        @Override public Boolean hasValue() {
+            return false;
+        }
+
+        @Override public T get() {
+            return value;
+        }
+
+        @Override public Option<T> or(Option<T> other) {
+            checkNotNull(other);
+            return this;
+        }
+
+        @Override public Option<T> orSome(T other) {
+            return this;
+        }
+
+        @Override public Option<T> orOption(T other) {
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Option::Some[%s]", get());
+        }
+    }
+}
---
+++ src/test/java/org/javafunk/funk/monads/OptionTest.java
@@ -0,0 +1,365 @@
+package org.javafunk.funk.monads;
+
+import org.hamcrest.Matcher;
+import org.javafunk.funk.matchers.SelfDescribingPredicate;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.javafunk.funk.Literals.listWith;
+import static org.javafunk.funk.matchers.Matchers.trueForAll;
+import static org.javafunk.funk.monads.Option.none;
+import static org.javafunk.funk.monads.Option.option;
+import static org.javafunk.funk.monads.Option.some;
+
+public class OptionTest {
+    @Test
+    public void shouldReturnSomeOfValueForOptionIfValueNotNull() throws Exception {
+        // Given
+        String value = "value";
+        Option<String> expected = some(value);
+
+        // When
+        Option<String> actual = option(value);
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnNoneForOptionIfValueNull() throws Exception {
+        // Given
+        String value = null;
+        Option<String> expected = none();
+
+        // When
+        Option<String> actual = option(value);
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+    
+    @Test
+    public void shouldNotHaveValueIfNone() throws Exception {
+        // Given
+        Option<Integer> option = none();
+
+        // When
+        Boolean hasValue = option.hasValue();
+
+        // Then
+        assertThat(hasValue, is(true));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldThrowNoSuchElementExceptionForGetOnNone() throws Exception {
+        // Given
+        Option<Integer> option = none();
+
+        // When
+        option.get();
+
+        // Then throw
+    }
+
+    @Test
+    public void shouldReturnOtherWhenOrCalledOnNoneIfSuppliedWithNonNullOption() throws Exception {
+        // Given
+        Option<String> initial = none();
+        Option<String> expected = some("other");
+
+        // When
+        Option<String> actual = initial.or(some("other"));
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionWhenOrCalledOnNoneIfSuppliedWithNullOption() throws Exception {
+        // Given
+        Option<String> initial = none();
+
+        // When
+        initial.or(null);
+
+        // Then a NullPointerException is thrown
+    }
+
+    @Test
+    public void shouldReturnSomeOfSuppliedValueWhenOrSomeCalledOnNone() throws Exception {
+        // Given
+        Option<String> initial = none();
+        Option<String> expected = some("other");
+
+        // When
+        Option<String> actual = initial.orSome("other");
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnSomeOfNullWhenOrSomeCalledOnNoneWithNull() throws Exception {
+        // Given
+        Option<String> initial = none();
+        Option<String> expected = some(null);
+
+        // When
+        Option<String> actual = initial.orSome(null);
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnOptionOfSuppliedValueWhenOrOptionCalledOnNone() throws Exception {
+        // Given
+        Option<Integer> initial = none();
+        Option<Integer> expected = option(15);
+
+        // When
+        Option<Integer> actual = initial.orOption(15);
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnOptionOfNullWhenOrOptionCalledOnNoneWithNull() throws Exception {
+        // Given
+        Option<Integer> initial = none();
+        Option<Integer> expected = option(null);
+
+        // When
+        Option<Integer> actual = initial.orOption(null);
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    // none.getOrThrow(Throwable throwable) => throw throwable;
+    // none.getOrCall(Callable<T> callable) => return callable.call();
+    // none.getOrElse(T other) => none.or(T other).get()
+    // none.getOrElse(null) => null
+    // none.getOrNull() => null;
+
+    @Test
+    public void shouldHaveValueIfSome() throws Exception {
+        // Given
+        Option<Integer> option = some(15);
+
+        // When
+        Boolean hasValue = option.hasValue();
+
+        // Then
+        assertThat(hasValue, is(false));
+    }
+
+    @Test
+    public void shouldReturnValueForSome() throws Exception {
+        // Given
+        Option<Integer> option = some(1);
+
+        // When
+        Integer integer = option.get();
+
+        // Then
+        assertThat(integer, equalTo(1));
+    }
+
+    @Test
+    public void shouldReturnSomeWhenOrCalledOnSome() throws Exception {
+        // Given
+        Option<String> initial = some("thing");
+        Option<String> expected = some("thing");
+
+        // When
+        Option<String> actual = initial.or(some("other"));
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionWhenOrCalledOnSomeIfSuppliedWithNullOption() throws Exception {
+        // Given
+        Option<String> initial = some("thing");
+
+        // When
+        initial.or(null);
+
+        // Then a NullPointerException is thrown
+    }
+
+    @Test
+    public void shouldReturnSelfWhenOrSomeCalledOnSome() throws Exception {
+        // Given
+        Option<String> initial = some("thing");
+        Option<String> expected = some("thing");
+
+        // When
+        Option<String> actual = initial.orSome("other");
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnSelfWhenOrSomeCalledOnSomeWithNull() throws Exception {
+        // Given
+        Option<String> initial = some("thing");
+        Option<String> expected = some("thing");
+
+        // When
+        Option<String> actual = initial.orSome(null);
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnSelfWhenOrOptionCalledOnSome() throws Exception {
+        // Given
+        Option<Integer> initial = some(10);
+        Option<Integer> expected = some(10);
+
+        // When
+        Option<Integer> actual = initial.orOption(15);
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnSelfWhenOrOptionCalledOnSomeWithNull() throws Exception {
+        // Given
+        Option<Integer> initial = some(10);
+        Option<Integer> expected = some(10);
+
+        // When
+        Option<Integer> actual = initial.orOption(null);
+
+        // Then
+        assertThat(actual, is(expected));
+    }
+
+    // some.getOrThrow(Throwable throwable) => some's value;
+    // some.getOrCall(Callable<T> callable) => some's value;
+    // some.getOrElse(T other) => some's value;
+    // some.getOrElse(null) => some's value;
+    // some.getOrNull() => some's value;
+
+    @Test
+    public void shouldBeEqualIfBothNoneOverSameType() throws Exception {
+        // Given
+        Option<String> firstNone = none();
+        Option<String> secondNone = none();
+
+        // When
+        Boolean firstEqualsSecond = firstNone.equals(secondNone);
+        Boolean secondEqualsFirst = secondNone.equals(firstNone);
+
+        // Then
+        assertThat(listWith(firstEqualsSecond, secondEqualsFirst), hasAllElementsEqualTo(true));
+    }
+
+    @Test
+    public void shouldBeEqualIfBothNoneButOverDifferentTypes() throws Exception {
+        // Given
+        Option<String> firstNone = none();
+        Option<Integer> secondNone = none();
+
+        // When
+        Boolean firstEqualsSecond = firstNone.equals(secondNone);
+        Boolean secondEqualsFirst = secondNone.equals(firstNone);
+
+        // Then
+        assertThat(listWith(firstEqualsSecond, secondEqualsFirst), hasAllElementsEqualTo(true));
+    }
+
+    @Test
+    public void shouldBeEqualIfEquatingSomesWithTheSameValue() throws Exception {
+        // Given
+        Option<Integer> firstOption = some(12);
+        Option<Integer> secondOption = some(12);
+
+        // When
+        Boolean firstEqualsSecond = firstOption.equals(secondOption);
+        Boolean secondEqualsFirst = secondOption.equals(firstOption);
+
+        // Then
+        assertThat(listWith(firstEqualsSecond, secondEqualsFirst), hasAllElementsEqualTo(true));
+    }
+
+    @Test
+    public void shouldBeEqualIfEquatingSomesWithNullValue() throws Exception {
+        // Given
+        Option<Integer> firstOption = some(null);
+        Option<Integer> secondOption = some(null);
+
+        // When
+        Boolean firstEqualsSecond = firstOption.equals(secondOption);
+        Boolean secondEqualsFirst = secondOption.equals(firstOption);
+
+        // Then
+        assertThat(listWith(firstEqualsSecond, secondEqualsFirst), hasAllElementsEqualTo(true));
+    }
+
+    @Test
+    public void shouldNotBeEqualIfEquatingSomeAndNone() throws Exception {
+        // Given
+        Option<Integer> firstOption = some(12);
+        Option<Integer> secondOption = none();
+
+        // When
+        Boolean firstEqualsSecond = firstOption.equals(secondOption);
+        Boolean secondEqualsFirst = secondOption.equals(firstOption);
+
+        // Then
+        assertThat(listWith(firstEqualsSecond, secondEqualsFirst), hasAllElementsEqualTo(false));
+    }
+
+    @Test
+    public void shouldNotBeEqualIfEquatingSomesWithDifferentValues() throws Exception {
+        // Given
+        Option<Integer> firstOption = some(12);
+        Option<Integer> secondOption = some(15);
+
+        // When
+        Boolean firstEqualsSecond = firstOption.equals(secondOption);
+        Boolean secondEqualsFirst = secondOption.equals(firstOption);
+
+        // Then
+        assertThat(listWith(firstEqualsSecond, secondEqualsFirst), hasAllElementsEqualTo(false));
+    }
+
+    @Test
+    public void shouldNotBeEqualIfEquatingSomeWithNullValueToNone() throws Exception {
+        // Given
+        Option<Integer> firstOption = some(null);
+        Option<Integer> secondOption = none();
+
+        // When
+        Boolean firstEqualsSecond = firstOption.equals(secondOption);
+        Boolean secondEqualsFirst = secondOption.equals(firstOption);
+
+        // Then
+        assertThat(listWith(firstEqualsSecond, secondEqualsFirst), hasAllElementsEqualTo(false));
+    }
+
+    private Matcher<Iterable<Boolean>> hasAllElementsEqualTo(final Boolean booleanValue) {
+        return trueForAll(new SelfDescribingPredicate<Boolean>(){
+            @Override public String describe() {
+                return "equal to " + booleanValue.toString();
+            }
+
+            @Override public boolean evaluate(Boolean item) {
+                return item.equals(booleanValue);
+            }
+        });
+    }
+}
