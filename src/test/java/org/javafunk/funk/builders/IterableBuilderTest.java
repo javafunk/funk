9b7cac1cf303d85c71a1dae21d9d69e638f376dd
Toby Clemson <tobyclemson@gmail.com>
2012-07-29 19:13:48 -0700

    Adding copyright notice to tests missing it.

--- src/test/java/org/javafunk/funk/builders/IterableBuilderTest.java
+++ src/test/java/org/javafunk/funk/builders/IterableBuilderTest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2011-Present Funk committers.
+ * All rights reserved.
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
 package org.javafunk.funk.builders;
 
 import org.junit.Test;
