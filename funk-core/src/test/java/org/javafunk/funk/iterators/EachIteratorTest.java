65ed4bee839edf81ed7ed80196bd96ae01892870
Toby Clemson <tobyclemson@gmail.com>
2013-06-29 15:58:05 -0700

    Adding toString to EachIterator.

--- funk-core/src/test/java/org/javafunk/funk/iterators/EachIteratorTest.java
+++ funk-core/src/test/java/org/javafunk/funk/iterators/EachIteratorTest.java
@@ -5,7 +5,12 @@ import org.junit.Test;
 
 import java.util.Iterator;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.javafunk.funk.Literals.iterableWith;
+import static org.javafunk.funk.Literals.iteratorWith;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class EachIteratorTest {
     @Test(expected = NullPointerException.class)
@@ -18,5 +23,50 @@ public class EachIteratorTest {
         new EachIterator<String>(input, action);
 
         // Then a NullPointerException is thrown.
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void shouldIncludeIteratorInToStringRepresentation() throws Exception {
+        // Given
+        Iterator<String> input = (Iterator<String>) mock(Iterator.class);
+        UnaryProcedure<String> action = new UnaryProcedure<String>() {
+            @Override public void execute(String value) {
+                System.out.println(value);
+            }
+        };
+
+        when(input.toString()).thenReturn("the-iterator");
+
+        EachIterator<String> iterator = new EachIterator<String>(input, action);
+
+        // When
+        String toString = iterator.toString();
+
+        // Then
+        assertThat(toString, containsString("the-iterator"));
+    }
+
+    @Test
+    public void shouldIncludeProcedureInToStringRepresentation() throws Exception {
+        // Given
+        Iterator<String> input = iteratorWith("1", "2", "3");
+        UnaryProcedure<String> action = new UnaryProcedure<String>() {
+            @Override public void execute(String value) {
+                System.out.println(value);
+            }
+
+            @Override public String toString() {
+                return "the-procedure";
+            }
+        };
+
+        EachIterator<String> iterator = new EachIterator<String>(input, action);
+
+        // When
+        String toString = iterator.toString();
+
+        // Then
+        assertThat(toString, containsString("the-procedure"));
     }
 }
