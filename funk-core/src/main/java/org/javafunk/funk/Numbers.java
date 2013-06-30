cdf5ec1e7070de93156fec49f44d0476026f1669
Toby Clemson <tobyclemson@gmail.com>
2013-06-29 22:40:39 -0700

    Adding Javadoc to all public methods in Numbers.

--- funk-core/src/main/java/org/javafunk/funk/Numbers.java
+++ funk-core/src/main/java/org/javafunk/funk/Numbers.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2011-Present Funk committers.
+ * All rights reserved.
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
 package org.javafunk.funk;
 
 import org.javafunk.funk.functors.Reducer;
@@ -15,26 +23,134 @@ import static org.javafunk.funk.monads.Option.option;
 public class Numbers {
     private Numbers() {}
 
+    /**
+     * Sums the supplied {@code Iterable} of {@code Number}s returning
+     * an instance of the supplied concrete {@code Number} type.
+     *
+     * <p>Currently, all elements in the supplied {@code Iterable}
+     * must be instances of the supplied concrete {@code Number} type
+     * although this may change in a future version of Funk.</p>
+     *
+     * <p>The only subtypes of {@code Number} currently supported are
+     * {@code Integer}, {@code Long}, {@code BigInteger}, {@code Float},
+     * {@code Double} and {@code BigDecimal} although more may be supported
+     * in a future version of Funk.</p>
+     *
+     * <p>In the case that the supplied iterable is empty, an
+     * {@code ArithmeticException} is thrown. If either the supplied
+     * {@code Iterable} or the supplied {@code Class} are {@code null},
+     * a {@code NullPointerException} is thrown.</p>
+     *
+     * @param iterable    An {@code Iterable} of {@code Number}s to be summed.
+     * @param numberClass A {@code Class} representing the concrete type of the
+     *                    {@code Number}s to be summed.
+     * @param <T>         The type of the {@code Number}s to be summed.
+     * @return The sum of the supplied numbers.
+     * @throws ArithmeticException  if the supplied {@code Iterable} is empty.
+     * @throws NullPointerException if either the supplied {@code Iterable} or
+     *                              the supplied {@code Class} are {@code null}.
+     */
     public static <T extends Number> T sumOrThrow(Iterable<T> iterable, Class<T> numberClass) {
         return process(iterable, numberClass, sumAccumulatorMap, "sum");
     }
 
+    /**
+     * Sums the supplied {@code Iterable} of {@code Number}s returning
+     * a {@code Some} over an instance of the supplied concrete
+     * {@code Number} type. In the case that the supplied iterable is empty,
+     * {@code None} is returned.
+     *
+     * <p>Currently, all elements in the supplied {@code Iterable}
+     * must be instances of the supplied concrete {@code Number} type
+     * although this may change in a future version of Funk.</p>
+     *
+     * <p>The only subtypes of {@code Number} currently supported are
+     * {@code Integer}, {@code Long}, {@code BigInteger}, {@code Float},
+     * {@code Double} and {@code BigDecimal} although more may be supported
+     * in a future version of Funk.</p>
+     *
+     * <p>If either the supplied {@code Iterable} or the supplied
+     * {@code Class} are {@code null}, a {@code NullPointerException} is
+     * thrown.</p>
+     *
+     * @param iterable    An {@code Iterable} of {@code Number}s to be summed.
+     * @param numberClass A {@code Class} representing the concrete type of the
+     *                    {@code Number}s to be summed.
+     * @param <T>         The type of the {@code Number}s to be summed.
+     * @return An {@code Option} over the sum of the supplied numbers.
+     * @throws NullPointerException if either the supplied {@code Iterable} or
+     *                              the supplied {@code Class} are {@code null}.
+     */
     public static <T extends Number> Option<T> sum(Iterable<T> iterable, Class<T> numberClass) {
         try {
             return option(sumOrThrow(iterable, numberClass));
-        } catch(ArithmeticException exception) {
+        } catch (ArithmeticException exception) {
             return none();
         }
     }
 
+    /**
+     * Multiplies the supplied {@code Iterable} of {@code Number}s
+     * returning an instance of the supplied concrete {@code Number} type.
+     *
+     * <p>Currently, all elements in the supplied {@code Iterable}
+     * must be instances of the supplied concrete {@code Number} type
+     * although this may change in a future version of Funk.</p>
+     *
+     * <p>The only subtypes of {@code Number} currently supported are
+     * {@code Integer}, {@code Long}, {@code BigInteger}, {@code Float},
+     * {@code Double} and {@code BigDecimal} although more may be supported
+     * in a future version of Funk.</p>
+     *
+     * <p>In the case that the supplied iterable is empty, an
+     * {@code ArithmeticException} is thrown. If either the supplied
+     * {@code Iterable} or the supplied {@code Class} are {@code null},
+     * a {@code NullPointerException} is thrown.</p>
+     *
+     * @param iterable    An {@code Iterable} of {@code Number}s to be multiplied.
+     * @param numberClass A {@code Class} representing the concrete type of the
+     *                    {@code Number}s to be multiplied.
+     * @param <T>         The type of the {@code Number}s to be multiplied.
+     * @return The product of the supplied numbers.
+     * @throws ArithmeticException  if the supplied {@code Iterable} is empty.
+     * @throws NullPointerException if either the supplied {@code Iterable} or
+     *                              the supplied {@code Class} are {@code null}.
+     */
     public static <T extends Number> T multiplyOrThrow(Iterable<T> iterable, Class<T> numberClass) {
         return process(iterable, numberClass, multiplyAccumulatorMap, "multiply");
     }
 
+    /**
+     * Multiplies the supplied {@code Iterable} of {@code Number}s
+     * returning a {@code Some} over an instance of the supplied concrete
+     * {@code Number} type. In the case that the supplied iterable is empty,
+     * {@code None} is returned.
+     *
+     * <p>Currently, all elements in the supplied {@code Iterable}
+     * must be instances of the supplied concrete {@code Number} type
+     * although this may change in a future version of Funk.</p>
+     *
+     * <p>The only subtypes of {@code Number} currently supported are
+     * {@code Integer}, {@code Long}, {@code BigInteger}, {@code Float},
+     * {@code Double} and {@code BigDecimal} although more may be supported
+     * in a future version of Funk.</p>
+     *
+     * <p>If either the supplied {@code Iterable} or the supplied
+     * {@code Class} are {@code null}, a {@code NullPointerException} is
+     * thrown.</p>
+     *
+     * @param iterable    An {@code Iterable} of {@code Number}s to be multiplied.
+     * @param numberClass A {@code Class} representing the concrete type of the
+     *                    {@code Number}s to be multiplied.
+     * @param <T>         The type of the {@code Number}s to be multiplied.
+     * @return An {@code Option} over the product of the supplied numbers.
+     * @throws NullPointerException if either the supplied {@code Iterable} or
+     *                              the supplied {@code Class} are {@code null}.
+     */
     public static <T extends Number> Option<T> multiply(Iterable<T> iterable, Class<T> numberClass) {
         try {
             return option(multiplyOrThrow(iterable, numberClass));
-        } catch(ArithmeticException exception) {
+        } catch (ArithmeticException exception) {
             return none();
         }
     }
