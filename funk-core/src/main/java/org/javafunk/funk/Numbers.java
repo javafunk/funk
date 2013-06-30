9cbbbedb8cf8b483a712214c5eb7a6c0c12c4bf5
Toby Clemson <tobyclemson@gmail.com>
2013-06-29 21:18:19 -0700

    Converting existing sum and multiply methods to throw on empty iterables and adding a variant of each that returns Option.

--- funk-core/src/main/java/org/javafunk/funk/Numbers.java
+++ funk-core/src/main/java/org/javafunk/funk/Numbers.java
@@ -1,42 +1,82 @@
 package org.javafunk.funk;
 
+import org.javafunk.funk.functors.Reducer;
+import org.javafunk.funk.monads.Option;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Map;
 
 import static java.lang.String.format;
-import static org.javafunk.funk.Accumulators.bigDecimalAdditionAccumulator;
-import static org.javafunk.funk.Accumulators.bigDecimalMultiplicationAccumulator;
-import static org.javafunk.funk.Accumulators.bigIntegerAdditionAccumulator;
-import static org.javafunk.funk.Accumulators.bigIntegerMultiplicationAccumulator;
-import static org.javafunk.funk.Accumulators.doubleAdditionAccumulator;
-import static org.javafunk.funk.Accumulators.doubleMultiplicationAccumulator;
-import static org.javafunk.funk.Accumulators.floatAdditionAccumulator;
-import static org.javafunk.funk.Accumulators.floatMultiplicationAccumulator;
-import static org.javafunk.funk.Accumulators.integerAdditionAccumulator;
-import static org.javafunk.funk.Accumulators.integerMultiplicationAccumulator;
-import static org.javafunk.funk.Accumulators.longAdditionAccumulator;
-import static org.javafunk.funk.Accumulators.longMultiplicationAccumulator;
+import static org.javafunk.funk.Accumulators.*;
+import static org.javafunk.funk.monads.Option.none;
+import static org.javafunk.funk.monads.Option.option;
 
 public class Numbers {
-    @SuppressWarnings("unchecked")
-    public static <T extends Number> T sum(Iterable<T> iterable, Class<T> numberClass) {
-        if (numberClass == Integer.class)    return (T) Eagerly.reduce((Iterable<Integer>)    iterable, integerAdditionAccumulator());
-        if (numberClass == Long.class)       return (T) Eagerly.reduce((Iterable<Long>)       iterable, longAdditionAccumulator());
-        if (numberClass == BigInteger.class) return (T) Eagerly.reduce((Iterable<BigInteger>) iterable, bigIntegerAdditionAccumulator());
-        if (numberClass == Float.class)      return (T) Eagerly.reduce((Iterable<Float>)      iterable, floatAdditionAccumulator());
-        if (numberClass == Double.class)     return (T) Eagerly.reduce((Iterable<Double>)     iterable, doubleAdditionAccumulator());
-        if (numberClass == BigDecimal.class) return (T) Eagerly.reduce((Iterable<BigDecimal>) iterable, bigDecimalAdditionAccumulator());
-        throw new UnsupportedOperationException(format("Cannot sum numbers of type: %s", numberClass.getSimpleName()));
+    private Numbers() {}
+
+    public static <T extends Number> T sumOrThrow(Iterable<T> iterable, Class<T> numberClass) {
+        return process(iterable, numberClass, sumAccumulatorMap, "sum");
     }
 
+    public static <T extends Number> Option<T> sum(Iterable<T> iterable, Class<T> numberClass) {
+        try {
+            return option(sumOrThrow(iterable, numberClass));
+        } catch(ArithmeticException exception) {
+            return none();
+        }
+    }
+
+    public static <T extends Number> T multiplyOrThrow(Iterable<T> iterable, Class<T> numberClass) {
+        return process(iterable, numberClass, multiplyAccumulatorMap, "multiply");
+    }
+
+    public static <T extends Number> Option<T> multiply(Iterable<T> iterable, Class<T> numberClass) {
+        try {
+            return option(multiplyOrThrow(iterable, numberClass));
+        } catch(ArithmeticException exception) {
+            return none();
+        }
+    }
+
+    private static final Map<Class<?>, Reducer<?, ?>> sumAccumulatorMap =
+            Literals.<Class<?>, Reducer<?, ?>>mapBuilder()
+                    .withKeyValuePair(Integer.class, integerAdditionAccumulator())
+                    .withKeyValuePair(Long.class, longAdditionAccumulator())
+                    .withKeyValuePair(BigInteger.class, bigIntegerAdditionAccumulator())
+                    .withKeyValuePair(Float.class, floatAdditionAccumulator())
+                    .withKeyValuePair(Double.class, doubleAdditionAccumulator())
+                    .withKeyValuePair(BigDecimal.class, bigDecimalAdditionAccumulator())
+                    .build();
+
+    private static final Map<Class<?>, Reducer<?, ?>> multiplyAccumulatorMap =
+            Literals.<Class<?>, Reducer<?, ?>>mapBuilder()
+                    .withKeyValuePair(Integer.class, integerMultiplicationAccumulator())
+                    .withKeyValuePair(Long.class, longMultiplicationAccumulator())
+                    .withKeyValuePair(BigInteger.class, bigIntegerMultiplicationAccumulator())
+                    .withKeyValuePair(Float.class, floatMultiplicationAccumulator())
+                    .withKeyValuePair(Double.class, doubleMultiplicationAccumulator())
+                    .withKeyValuePair(BigDecimal.class, bigDecimalMultiplicationAccumulator())
+                    .build();
+
     @SuppressWarnings("unchecked")
-    public static <T extends Number> T multiply(Iterable<T> iterable, Class<T> numberClass) {
-        if (numberClass == Integer.class)    return (T) Eagerly.reduce((Iterable<Integer>)    iterable, integerMultiplicationAccumulator());
-        if (numberClass == Long.class)       return (T) Eagerly.reduce((Iterable<Long>)       iterable, longMultiplicationAccumulator());
-        if (numberClass == BigInteger.class) return (T) Eagerly.reduce((Iterable<BigInteger>) iterable, bigIntegerMultiplicationAccumulator());
-        if (numberClass == Float.class)      return (T) Eagerly.reduce((Iterable<Float>)      iterable, floatMultiplicationAccumulator());
-        if (numberClass == Double.class)     return (T) Eagerly.reduce((Iterable<Double>)     iterable, doubleMultiplicationAccumulator());
-        if (numberClass == BigDecimal.class) return (T) Eagerly.reduce((Iterable<BigDecimal>) iterable, bigDecimalMultiplicationAccumulator());
-        throw new UnsupportedOperationException(format("Cannot multiply numbers of type: %s", numberClass.getSimpleName()));
+    private static <T extends Number> T process(
+            Iterable<T> iterable,
+            Class<T> numberClass,
+            Map<Class<?>, Reducer<?, ?>> reducerMap,
+            String operation) {
+        Checks.returnOrThrowIfNull(iterable,
+                new NullPointerException(format("Cannot %s a null collection.", operation)));
+        Checks.returnOrThrowIfNull(numberClass,
+                new NullPointerException(format("Cannot %s when a null number class is provided.", operation)));
+        Checks.returnOrThrowIfEmpty(iterable,
+                new ArithmeticException(format("Cannot %s a collection containing no numbers.", operation)));
+
+        if (reducerMap.containsKey(numberClass)) {
+            return (T) Eagerly.reduce(iterable, (Reducer<Object, Object>) reducerMap.get(numberClass));
+        } else {
+            throw new UnsupportedOperationException(
+                    format("Cannot %s numbers of type: %s", operation, numberClass.getSimpleName()));
+        }
     }
 }
