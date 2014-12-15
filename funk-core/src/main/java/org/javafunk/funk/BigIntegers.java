8d09b700150a8b5349ee785f63b7fdebe4c44a0a
Toby Clemson <tobyclemson@gmail.com>
2014-12-15 01:16:35 -0500

    Add more conversion methods to BigIntegers.

--- funk-core/src/main/java/org/javafunk/funk/BigIntegers.java
+++ funk-core/src/main/java/org/javafunk/funk/BigIntegers.java
@@ -2,13 +2,54 @@ package org.javafunk.funk;
 
 import org.javafunk.funk.functors.Mapper;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class BigIntegers {
+    public static Mapper<String, BigInteger> fromStringToBigInteger() {
+        return new Mapper<String, BigInteger>() {
+            @Override public BigInteger map(String input) {
+                return new BigInteger(input);
+            }
+        };
+    }
+
+    public static Mapper<Integer, BigInteger> fromIntegerToBigInteger() {
+        return new Mapper<Integer, BigInteger>() {
+            @Override public BigInteger map(Integer input) {
+                return new BigInteger(input.toString());
+            }
+        };
+    }
+
+    public static Mapper<Long, BigInteger> fromLongToBigInteger() {
+        return new Mapper<Long, BigInteger>() {
+            @Override public BigInteger map(Long input) {
+                return new BigInteger(input.toString());
+            }
+        };
+    }
+
     public static Mapper<Float, BigInteger> fromFloatToBigInteger() {
         return new Mapper<Float, BigInteger>() {
             @Override public BigInteger map(Float input) {
                 return new BigInteger(((Integer) input.intValue()).toString());
+            }
+        };
+    }
+
+    public static Mapper<Double, BigInteger> fromDoubleToBigInteger() {
+        return new Mapper<Double, BigInteger>() {
+            @Override public BigInteger map(Double input) {
+                return new BigInteger(((Long) input.longValue()).toString());
+            }
+        };
+    }
+
+    public static Mapper<BigDecimal, BigInteger> fromBigDecimalToBigInteger() {
+        return new Mapper<BigDecimal, BigInteger>() {
+            @Override public BigInteger map(BigDecimal input) {
+                return input.toBigInteger();
             }
         };
     }
--- funk-core/src/test/java/org/javafunk/funk/BigIntegersTest.java
+++ funk-core/src/test/java/org/javafunk/funk/BigIntegersTest.java
@@ -2,14 +2,44 @@ package org.javafunk.funk;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import static org.hamcrest.Matchers.is;
-import static org.javafunk.funk.BigIntegers.fromFloatToBigInteger;
-import static org.javafunk.funk.BigIntegers.toStringValue;
+import static org.javafunk.funk.BigIntegers.*;
 import static org.junit.Assert.assertThat;
 
 public class BigIntegersTest {
+    @Test
+    public void returnsMapperFromStringToBigInteger() {
+        assertThat(fromStringToBigInteger().map("12345678"), is(new BigInteger("12345678")));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwsNullPointerExceptionWhenMapperFromStringToBigIntegerPassedNull() {
+        fromStringToBigInteger().map(null);
+    }
+
+    @Test
+    public void returnsMapperFromIntegerToBigInteger() {
+        assertThat(fromIntegerToBigInteger().map(1234), is(new BigInteger("1234")));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwsNullPointerExceptionWhenMapperFromIntegerToBigIntegerPassedNull() {
+        fromIntegerToBigInteger().map(null);
+    }
+
+    @Test
+    public void returnsMapperFromLongToBigInteger() {
+        assertThat(fromLongToBigInteger().map(1234L), is(new BigInteger("1234")));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwsNullPointerExceptionWhenMapperFromLongToBigIntegerPassedNull() {
+        fromLongToBigInteger().map(null);
+    }
+
     @Test
     public void returnsMapperFromFloatToBigInteger() {
         assertThat(fromFloatToBigInteger().map(1234.56F), is(new BigInteger("1234")));
@@ -18,6 +48,26 @@ public class BigIntegersTest {
     @Test(expected = NullPointerException.class)
     public void throwsNullPointerExceptionWhenMapperFromFloatToBigIntegerPassedNull() {
         fromFloatToBigInteger().map(null);
+    }
+
+    @Test
+    public void returnsMapperFromDoubleToBigInteger() {
+        assertThat(fromDoubleToBigInteger().map(1234.56D), is(new BigInteger("1234")));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwsNullPointerExceptionWhenMapperFromDoubleToBigIntegerPassedNull() {
+        fromDoubleToBigInteger().map(null);
+    }
+
+    @Test
+    public void returnsMapperFromBigDecimalToBigInteger() {
+        assertThat(fromBigDecimalToBigInteger().map(new BigDecimal("1234.56")), is(new BigInteger("1234")));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwsNullPointerExceptionWhenMapperFromBigDecimalToBigIntegerPassedNull() {
+        fromBigDecimalToBigInteger().map(null);
     }
 
     @Test
